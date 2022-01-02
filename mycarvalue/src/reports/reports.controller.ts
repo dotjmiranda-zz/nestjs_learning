0362a11573b1ff3d789fc7c357d9557846981000
João Miranda <joaomiranda@fastmail.com>
2022-01-02 12:38:30 +0000

    added basic authorization and reports logic for estimates

--- mycarvalue/src/reports/reports.controller.ts
+++ mycarvalue/src/reports/reports.controller.ts
@@ -1,6 +1,22 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { AdminGuard } from 'src/guards/admin.guard';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
+import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
 
 @Controller('reports')
@@ -8,8 +24,20 @@ export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   @Post()
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
+  }
+
+  @UseGuards(AdminGuard)
+  @Patch(':id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query);
   }
 }
